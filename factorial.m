489750c71e88506e367e63ee168a2abefd7dfb97
Dondarrion17 <sskapek@gmail.com>
2017-12-11 18:05:37 -0600

    Made Better
    Changed rounding to Int during Galton board instead of at the end for
    massive improvement in accuracy

--- factorial.m
+++ factorial.m
@@ -5,14 +5,14 @@ if n == 1
 else
     fact = galton(n)*factorial(n-1);
 end
-fact = round(fact,0);
+%fact = round(fact,0);
 end
 
 function [ g ] = galton(n)
 %Creates a decent estimation of n choose 1
 total = 0;
-%If one row of binomial sums to 2^n, we have it 16 times
-for i = 1:2^(n+12)
+%If one row of binomial sums to 2^n, we have it 256 times
+for i = 1:2^(n+8)
     sum = 0;
     for k = 1:n
         sum = sum + randi([0,1],1);
@@ -21,6 +21,6 @@ for i = 1:2^(n+12)
         total = total + 1;
     end
 end
-g = total/(2^12);
+g = round(total/(2^8),0);
 end
 
